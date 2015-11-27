cc61ac6e95779b63fcd970bd1f8776f9a1a8cfaa
iainbrighton <iainbrighton@hotmail.com>
2015-11-27 23:05:32 +0000

    Documentation updates

--- Src/Lab.ps1
+++ Src/Lab.ps1
@@ -1,7 +1,32 @@
 function Start-Lab {
 <#
-    .SYNOPSIS
-        Starts all nodes in a DSC configuration document.
+     .SYNOPSIS
+        Starts all VMs in a lab in a predefined order.
+    .DESCRIPTION
+        The Start-Lab cmdlet starts all nodes defined in a PowerShell DSC configuration document, in a preconfigured
+        order.
+        
+        Unlike the standard Start-VM cmdlet, the Start-Lab cmdlet will read the specified PowerShell DSC configuration
+        document and infer the required start up order.
+        
+        The PowerShell DSC configuration document can define the start/stop order of the virtual machines and the boot
+        delay between each VM power operation. This is defined with the BootOrder and BootDelay properties. The lower
+        the virtual machine's BootOrder index, the earlier it is started (in relation to the other VMs).
+        
+        For example, a VM with a BootOrder index of 10 will be started before a VM with a BootOrder index of 11. All
+        virtual machines receive a BootOrder value of 99 unless specified otherwise.
+
+        The delay between each power operation is defined with the BootDelay property. This value is specified in
+        seconds and is enforced between starting or stopping a virtual machine.
+        
+        For example, a VM with a BootDelay of 30 will enforce a 30 second delay after being powered on or after the
+        power off command is issued. All VMs receive a BootDelay value of 0 (no delay) unless specified otherwise.       
+    .PARAMETER ConfigurationData
+        Specifies a PowerShell DSC configuration data hashtable or a path to an existing PowerShell DSC .psd1
+        configuration document.
+    .LINK
+        about_ConfigurationData
+        Stop-Lab
 #>
     [CmdletBinding()]
     param (
@@ -33,8 +58,33 @@ function Start-Lab {
 
 function Stop-Lab {
 <#
-    .SYNOPSIS
-        Stops all nodes in a DSC configuration document.
+     .SYNOPSIS
+        Stops all VMs in a lab in a predefined order.
+    .DESCRIPTION
+        The Stop-Lab cmdlet stops all nodes defined in a PowerShell DSC configuration document, in a preconfigured
+        order.
+        
+        Unlike the standard Stop-VM cmdlet, the Stop-Lab cmdlet will read the specified PowerShell DSC configuration
+        document and infer the required shutdown order.
+        
+        The PowerShell DSC configuration document can define the start/stop order of the virtual machines and the boot
+        delay between each VM power operation. This is defined with the BootOrder and BootDelay properties. The higher
+        the virtual machine's BootOrder index, the earlier it is stopped (in relation to the other VMs).
+        
+        For example, a VM with a BootOrder index of 11 will be stopped before a VM with a BootOrder index of 10. All
+        virtual machines receive a BootOrder value of 99 unless specified otherwise.
+
+        The delay between each power operation is defined with the BootDelay property. This value is specified in
+        seconds and is enforced between starting or stopping a virtual machine.
+        
+        For example, a VM with a BootDelay of 30 will enforce a 30 second delay after being powered on or after the
+        power off command is issued. All VMs receive a BootDelay value of 0 (no delay) unless specified otherwise.       
+    .PARAMETER ConfigurationData
+        Specifies a PowerShell DSC configuration data hashtable or a path to an existing PowerShell DSC .psd1
+        configuration document.
+    .LINK
+        about_ConfigurationData
+        Start-Lab
 #>
     [CmdletBinding()]
     param (
@@ -66,10 +116,26 @@ function Stop-Lab {
 
 function Reset-Lab {
 <#
-    .SYNOPSIS
-        Reverts a lab back to it's intial configuration.
+     .SYNOPSIS
+        Reverts all VMs in a lab back to their initial configuration.
+    .DESCRIPTION
+        The Reset-Lab cmdlet will reset all the nodes defined in a PowerShell DSC configuration document, back to their
+        initial state. If virtual machines are powered on, they will automatically be powered off when restoring the
+        snapshot.
+
+        When virtual machines are created - before they are powered on - a baseline snapshot is created. This snapshot
+        is taken before the Sysprep process has been run and/or any PowerShell DSC configuration has been applied.
+        
+        WARNING: You will lose all changes to all virtual machines that have not been committed via another snapshot.
+    .PARAMETER ConfigurationData
+        Specifies a PowerShell DSC configuration data hashtable or a path to an existing PowerShell DSC .psd1
+        configuration document.
+    .LINK
+        Checkpoint-Lab
     .NOTES
-        This uses the baseline snapshot. If they don't exist, the lab can also be reconfigured with Start-LabConfiguration -Force.
+        This cmdlet uses the baseline snapshot snapshot created by the Start-LabConfiguration cmdlet. If the baseline
+        was not created or the baseline snapshot does not exist, the lab VMs can be recreated with the
+        Start-LabConfiguration -Force.
 #>
     [CmdletBinding()]
     param (
@@ -90,7 +156,27 @@ function Reset-Lab {
 function Checkpoint-Lab {
 <#
     .SYNOPSIS
-        Snapshots a lab in it's current configuration with the supplied name/description.
+        Snapshots all lab VMs in their current configuration.
+    .DESCRIPTION
+        The Checkpoint-Lab creates a VM checkpoint of all the nodes defined in a PowerShell DSC configuration document.
+        When creating the snapshots, they will be created using the snapshot name specified.
+
+        All virtual machines should be powered off when the snapshot is taken to ensure that the machine is in a
+        consistent state. If VMs are powered on, an error will be generated. You can override this behaviour by
+        specifying the -Force parameter.
+
+        WARNING: If the -Force parameter is used, the virtual machine snapshot(s) may be in an inconsistent state.
+    .PARAMETER ConfigurationData
+        Specifies a PowerShell DSC configuration data hashtable or a path to an existing PowerShell DSC .psd1
+        configuration document.
+    .PARAMETER SnapshotName
+        Specifies the virtual machine snapshot name that applied to each VM in the PowerShell DSC configuration
+        document. This name is used to restore a lab configuration. It can contain spaces, but is not recommended.
+    .PARAMETER Force
+        Forces virtual machine snapshots to be taken - even if there are any running virtual machines.
+    .LINK
+        Restore-Lab
+        Reset-Lab
 #>
     [CmdletBinding()]
     param (
@@ -125,7 +211,29 @@ function Checkpoint-Lab {
 function Restore-Lab {
 <#
     .SYNOPSIS
-        Reverts a lab to a previous snapshot name/description.
+        Restores all lab VMs to a previous configuration.
+    .DESCRIPTION
+        The Restore-Lab reverts all the nodes defined in a PowerShell DSC configuration document, back to a
+        previously captured configuration.
+
+        When creating the snapshots, they are created using a snapshot name. To restore a lab to a previous
+        configuration, you must supply the same snapshot name.
+
+        All virtual machines should be powered off when the snapshots are restored. If VMs are powered on,
+        an error will be generated. You can override this behaviour by specifying the -Force parameter.
+
+        WARNING: If the -Force parameter is used, running virtual machines will be powered off automatically.
+    .PARAMETER ConfigurationData
+        Specifies a PowerShell DSC configuration data hashtable or a path to an existing PowerShell DSC .psd1
+        configuration document.
+    .PARAMETER SnapshotName
+        Specifies the virtual machine snapshot name to be restored. You must use the same snapshot name used when
+        creating the snapshot with the Checkpoint-Lab cmdlet.
+    .PARAMETER Force
+        Forces virtual machine snapshots to be restored - even if there are any running virtual machines.
+    .LINK
+        Checkpoint-Lab
+        Reset-Lab
 #>
     [CmdletBinding()]
     param (
--- Src/LabConfiguration.ps1
+++ Src/LabConfiguration.ps1
@@ -1,7 +1,21 @@
 function Test-LabConfiguration {
 <#
     .SYNOPSIS
-        Invokes a lab configuration from a DSC configuration document.
+        Tests the configuration of all VMs in a lab.
+    .DESCRIPTION
+        The Test-LabConfiguration determines whether all nodes defined in a PowerShell DSC configuration document
+        are configured correctly and returns the results.
+
+        WANRING: Only the virtual machine configuration is checked, not in the internal VM configuration. For example,
+        the virtual machine's memory configuraiton, virtual switch configuration and processor count are tested. The
+        VM's operating system configuration is not checked.
+    .PARAMETER ConfigurationData
+        Specifies a PowerShell DSC configuration data hashtable or a path to an existing PowerShell DSC .psd1
+        configuration document used to create the virtual machines. Each node defined in the AllNodes array is
+        tested.
+    .LINK
+        about_ConfigurationData
+        Start-LabConfiguration
 #>
     [CmdletBinding()]
     param (
@@ -71,7 +85,82 @@ function TestLabConfigurationMof {
 function Start-LabConfiguration {
 <#
     .SYNOPSIS
-        Invokes a lab configuration from a DSC configuration document.
+        Invokes the deployment and configuration of a VM for each node defined in a PowerShell DSC configuration
+        document.
+    .DESCRIPTION
+        The Start-LabConfiguration initiates the configuration of all nodes defined in a PowerShell DSC configuration
+        document. The AllNodes array is enumerated and a virtual machine is created per node, using the NodeName
+        property.
+
+        If any existing virtual machine exists with the same name as the NodeName declaration of the AllNodes array,
+        the cmdlet will throw an error. If this behaviour is not desired, you can forcibly remove the existing VM
+        with the -Force parameter. NOTE: THE VIRTUAL MACHINE'S EXISTING DATA WILL BE LOST.
+
+        The virtual machines' local administrator password must be specified when creating the lab VMs. The local
+        administrator password can be specified as a [PSCredential] or a [SecureString] type. If a [PSCredential] is
+        used then the username is not used.
+
+        It is possible to override the module's virtual machine default settings by specifying the required property
+        on the node hashtable in the PowerShell DSC configuration document. Default settings include the Operating
+        System image to use, the amount of memory assigned to the virtual machine and/or the virtual switch to
+        connect the virtual machine to. If the settings are not overridden, the module's defaults are used. Use the
+        Get-LabVMDefaults cmdlet to view the module's default values.
+        
+        Each virtual machine created by the Start-LabConfiguration cmdlet, has its PowerShell DSC configuraion (.mof)
+        file injected into the VHD file as it is created. This configuration is then applied during the first boot
+        process to ensure the virtual machine is configured as required. If the path to the VM's .mof files is not
+        specified, the module's default Configuration directory is used. Use the Get-LabHostDefaults cmdlet to view
+        the module's default Configuration directory path.
+        
+        The virtual machine .mof files must be created before creating the lab. If any .mof files are missing, the
+        Start-LabConfiguration cmdlet will generate an error. You can choose to ignore this error by specifying
+        the -SkipMofCheck parameter. If you skip the .mof file check - and no .mof file is found - no configuration
+        will be applied to the virtual machine's Operating System settings.
+
+        When deploying a lab, the module will create a default baseline snapshot of all virtual machines. This
+        snapshot can be used to revert all VMs back to their default configuration. If this snapshot is not
+        required, it can be surpressed with the -NoSnapshot parameter.
+    .NOTES
+        The same local administrator password is used for all virtual machines created in the same lab configuration.
+    .PARAMETER ConfigurationData
+        Specifies a PowerShell DSC configuration data hashtable or a path to an existing PowerShell DSC .psd1
+        configuration document used to create the virtual machines. One virtual machine is created per node defined
+        in the AllNodes array.
+    .PARAMETER Credential
+        Specifies the local administrator password of all virtual machines in the lab configuration. The same
+        password is used for all virtual machines in the same lab configuration. The username is not used.
+    .PARAMETER Password
+        Specifies the local administrator password of all virtual machines in the lab configuration. The same
+        password is used for all virtual machines in the same lab configuration.
+    .PARAMETER Path
+        Specifies the directory path containing the individual PowerShell DSC .mof files. If not specified, the
+        module's default location is used.
+    .PARAMETER NoSnapshot
+        Specifies that no default snapshot will be taken when creating the virtual machine.
+        
+        NOTE: If no default snapshot is not created, the lab cannot be restored to its initial configuration
+        with the Reset-Lab cmdlet.
+    .PARAMETER SkipMofCheck
+        Specifies that the module will configure a virtual machines that do not have a corresponding .mof file
+        located in the -Path specfified. By default, if any .mof file cannot be located then the cmdlet will
+        generate an error.
+        
+        NOTE: If no .mof file is found and the -SkipMofCheck parameter is specified, no configuration will be
+        applied to the virtual machine's Operating System configuration.
+    .PARAMETER Force
+        Specifies that any existing virtual machine with a matching name, will be removed and recreated. By
+        default, if a virtual machine already exists with the same name, the cmdlet will generate an error.
+        
+        NOTE: If the -Force parameter is specified - and a virtual machine with the same name already exists -
+        ALL EXISTING DATA WITHIN THE VM WILL BE LOST.
+    .LINK
+        about_ConfigurationData
+        about_Bootstrap
+        Get-LabHostDefaults
+        Get-LabHostDefaults
+        Get-LabVMDefaults
+        Set-LabVMDefaults
+        Reset-Lab
 #>
     [CmdletBinding(DefaultParameterSetName = 'PSCredential')]
     param (
@@ -144,7 +233,25 @@ function Start-LabConfiguration {
 function Remove-LabConfiguration {
 <#
     .SYNOPSIS
-        Removes a lab configuration from a DSC configuration document.
+        Removes all VMs and associated snapshots of all nodes defined in a PowerShell DSC configuration document.
+    .DESCRIPTION
+        The Remove-LabConfiguration removes all virtual machines that have a corresponding NodeName defined in the
+        AllNode array of the PowerShell DSC configuration document.
+
+        WARNING: ALL EXISTING VIRTUAL MACHINE DATA WILL BE LOST WHEN VIRTUAL MACHINES ARE REMOVED.
+
+        By default, associated virtual machine switches are not removed as they may be used by other virtual
+        machines or lab configurations. If you wish to remove any virtual switche defined in the PowerShell DSC
+        configuration document, specify the -RemoveSwitch parameter.
+    .PARAMETER ConfigurationData
+        Specifies a PowerShell DSC configuration data hashtable or a path to an existing PowerShell DSC .psd1
+        configuration document used to remove existing virtual machines. One virtual machine is removed per node
+        defined in the AllNodes array.
+    .PARAMETER RemoveSwitch
+        Specifies that any connected virtual switch should also be removed when the virtual machine is removed.
+    .LINK
+        about_ConfigurationData
+        Start-LabConfiguration
 #>
     [CmdletBinding()]
     param (
@@ -161,6 +268,7 @@ function Remove-LabConfiguration {
         WriteVerbose $localized.StartedLabConfiguration;
         $nodes = $ConfigurationData.AllNodes | Where { $_.NodeName -ne '*' };
         foreach ($node in $nodes) {
+            ##TODO: Should this not ensure that VMs are powered off?
             RemoveLabVM -Name $node.NodeName -ConfigurationData $ConfigurationData -RemoveSwitch:$RemoveSwitch;
         }
         WriteVerbose $localized.FinishedLabConfiguration;
