f931d21ce6d8e0bf089782cdf860bf7674a3412e
csandfeld <csa@sol.dk>
2015-11-29 22:21:45 +0100

    Force order of snapshot removal to descending creation time (newest first)

--- Src/LabVMSnapshot.ps1
+++ Src/LabVMSnapshot.ps1
@@ -12,7 +12,9 @@ function RemoveLabVMSnapshot {
        <## TODO: Add the ability to force/wait for the snapshots to be removed. When removing snapshots it take a minute
                  or two before the files are actually removed. This causes issues when performing a lab reset #>
         foreach ($vmName in $Name) {
-            Get-VMSnapshot -VMName $vmName -ErrorAction SilentlyContinue | Where Name -like $SnapshotName | ForEach-Object {
+            
+            # Sort by descending CreationTime to ensure we will not have to commit changes from one snapshot to another
+            Get-VMSnapshot -VMName $vmName -ErrorAction SilentlyContinue | Where Name -like $SnapshotName | Sort-Object -Property CreationTime -Descending | ForEach-Object {
                 WriteVerbose ($localized.RemovingSnapshot -f $vmName, $_.Name);
                 Remove-VMSnapshot -VMName $_.VMName -Name $_.Name;
             }
