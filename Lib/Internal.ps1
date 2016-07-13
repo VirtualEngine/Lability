b5b4e7cdbbea65a12a67461f22f1b9881a72c21e
Iain Brighton <iainbrighton@hotmail.com>
2016-07-13 17:04:31 +0100

    Adds ResolveProgramFilesFolder function

--- Lib/Internal.ps1
+++ Lib/Internal.ps1
@@ -360,3 +360,54 @@ function ValidateTimeZone {
         }
     } #end process
 } #end function ValidateTimeZone
+
+
+function ResolveProgramFilesFolder {
+<#
+    .SYNOPSIS
+        Resolves known localized %ProgramFiles% directories.
+    .LINK
+        https://en.wikipedia.org/wiki/Program_Files
+#>
+    [CmdletBinding(DefaultParameterSetName = 'Path')]
+    [OutputType([System.IO.DirectoryInfo])]
+    param (
+        ## Root path to check
+        [Parameter(Mandatory, ParameterSetName = 'Path')]
+        [ValidateNotNullOrEmpty()]
+        [System.String] $Path,
+
+        ## Drive letter
+        [Parameter(Mandatory, ParameterSetName = 'Drive')]
+        [ValidateLength(1,1)]
+        [System.String] $Drive
+    )
+    begin {
+
+        if ($PSCmdlet.ParameterSetName -eq 'Drive') {
+            $Path = '{0}:\' -f $Drive;
+        }
+
+    }
+    process {
+        $knownFolderNames = @(
+            "Program Files",
+            "Programmes",
+            "Archivos de programa",
+            "Programme",
+            "Programfájlok",
+            "Programmi",
+            "Programmer",
+            "Program",
+            "Programfiler",
+            "Arquivos de Programas",
+            "Programas"
+            "Αρχεία Εφαρμογών"
+        )
+
+        Get-ChildItem -Path $Path -Directory |
+            Where-Object Name -in $knownFolderNames |
+                Select-Object -First 1;
+
+    } #end process
+} #end function ResolveProgramFilesFolder
