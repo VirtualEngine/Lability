56625e0519a3b113966bfb292f93ee93b08d6832
Iain Brighton <iainbrighton@hotmail.com>
2017-07-19 14:19:21 +0100

    Removes deprecates cmdlets

--- Tests/Unit/Src/Public/Checkpoint-Lab.Tests.ps1
+++ Tests/Unit/Src/Public/Checkpoint-Lab.Tests.ps1
@@ -14,84 +14,84 @@ Describe 'Unit\Src\Public\Checkpoint-Lab' {
         Mock New-LabVMSnapshot -MockWith { }
 
         It 'Snapshots VMs when powered off' {
-                $configurationData = @{
-                    AllNodes = @(
-                        @{ NodeName = 'VM1'; }, @{ NodeName = 'VM2'; }, @{ NodeName = 'VM3'; }
-                    )
-                }
-                $testSnapshotName = 'Test Snapshot';
-                Mock Get-VM -MockWith { return [PSCustomObject] @{ State = 'Off' }; }
-
-                Checkpoint-Lab -ConfigurationData $configurationData -SnapshotName $testSnapshotName;
-
-                Assert-MockCalled New-LabVMSnapshot -ParameterFilter { $SnapshotName -eq $testSnapshotName };
+            $configurationData = @{
+                AllNodes = @(
+                    @{ NodeName = 'VM1'; }, @{ NodeName = 'VM2'; }, @{ NodeName = 'VM3'; }
+                )
             }
+            $testSnapshotName = 'Test Snapshot';
+            Mock Get-VM -MockWith { return [PSCustomObject] @{ State = 'Off' }; }
 
-            It 'Snapshots VM using display name' {
-                $testVMName = 'VM1';
-                $testEnvironmentPrefix = 'Test_';
-                $testVMDisplayName = '{0}{1}' -f $testEnvironmentPrefix, $testVMName;
-                $configurationData = @{
-                    AllNodes = @(
-                        @{ NodeName = $testVMName; }
-                    )
-                    NonNodeData = @{
-                        Lability = @{
-                            EnvironmentPrefix = $testEnvironmentPrefix;
-                        }
+            Checkpoint-Lab -ConfigurationData $configurationData -SnapshotName $testSnapshotName;
+
+            Assert-MockCalled New-LabVMSnapshot -ParameterFilter { $SnapshotName -eq $testSnapshotName };
+        }
+
+        It 'Snapshots VM using display name' {
+            $testVMName = 'VM1';
+            $testEnvironmentPrefix = 'Test_';
+            $testVMDisplayName = '{0}{1}' -f $testEnvironmentPrefix, $testVMName;
+            $configurationData = @{
+                AllNodes = @(
+                    @{ NodeName = $testVMName; }
+                )
+                NonNodeData = @{
+                    Lability = @{
+                        EnvironmentPrefix = $testEnvironmentPrefix;
                     }
                 }
-                $testSnapshotName = 'Test Snapshot';
-                Mock Get-VM -MockWith { return [PSCustomObject] @{ State = 'Off' }; }
-
-                Checkpoint-Lab -ConfigurationData $configurationData -SnapshotName $testSnapshotName;
-
-                Assert-MockCalled New-LabVMSnapshot -ParameterFilter { $Name -eq $testVMDisplayName };
             }
+            $testSnapshotName = 'Test Snapshot';
+            Mock Get-VM -MockWith { return [PSCustomObject] @{ State = 'Off' }; }
 
-            It 'Errors when there is one running VM' {
-                $configurationData = @{
-                    AllNodes = @(
-                        @{ NodeName = 'VM1'; }, @{ NodeName = 'VM2'; }, @{ NodeName = 'VM3'; }
-                    )
-                }
-                $testSnapshotName = 'Test Snapshot';
-                $fakeOffVM = [PSCustomObject] @{ State = 'Off' };
-                $fakeRunningVM = [PSCustomObject] @{ State = 'Running' };
-                Mock Get-VM -MockWith { return @($fakeRunningVM, $fakeOffVM, $fakeOffVM); }
+            Checkpoint-Lab -ConfigurationData $configurationData -SnapshotName $testSnapshotName;
 
-                { Checkpoint-Lab -ConfigurationData $configurationData -SnapshotName $testSnapshotName -ErrorAction Stop } | Should Throw;
+            Assert-MockCalled New-LabVMSnapshot -ParameterFilter { $Name -eq $testVMDisplayName };
+        }
+
+        It 'Errors when there is one running VM' {
+            $configurationData = @{
+                AllNodes = @(
+                    @{ NodeName = 'VM1'; }, @{ NodeName = 'VM2'; }, @{ NodeName = 'VM3'; }
+                )
             }
+            $testSnapshotName = 'Test Snapshot';
+            $fakeOffVM = [PSCustomObject] @{ State = 'Off' };
+            $fakeRunningVM = [PSCustomObject] @{ State = 'Running' };
+            Mock Get-VM -MockWith { return @($fakeRunningVM, $fakeOffVM, $fakeOffVM); }
 
-            It 'Errors when there are multiple running VMs' {
-                $configurationData = @{
-                    AllNodes = @(
-                        @{ NodeName = 'VM1'; }, @{ NodeName = 'VM2'; }, @{ NodeName = 'VM3'; }
-                    )
-                }
-                $testSnapshotName = 'Test Snapshot';
-                $fakeOffVM = [PSCustomObject] @{ State = 'Off' };
-                $fakeRunningVM = [PSCustomObject] @{ State = 'Running' };
-                Mock Get-VM -MockWith { return @($fakeRunningVM, $fakeOffVM, $fakeRunningVM); }
+            { Checkpoint-Lab -ConfigurationData $configurationData -SnapshotName $testSnapshotName -ErrorAction Stop } | Should Throw;
+        }
 
-                { Checkpoint-Lab -ConfigurationData $configurationData -SnapshotName $testSnapshotName -ErrorAction Stop } | Should Throw;
+        It 'Errors when there are multiple running VMs' {
+            $configurationData = @{
+                AllNodes = @(
+                    @{ NodeName = 'VM1'; }, @{ NodeName = 'VM2'; }, @{ NodeName = 'VM3'; }
+                )
             }
+            $testSnapshotName = 'Test Snapshot';
+            $fakeOffVM = [PSCustomObject] @{ State = 'Off' };
+            $fakeRunningVM = [PSCustomObject] @{ State = 'Running' };
+            Mock Get-VM -MockWith { return @($fakeRunningVM, $fakeOffVM, $fakeRunningVM); }
 
-            It 'Snapshots VMs when there are running VMs and -Force is specified' {
-                $configurationData = @{
-                    AllNodes = @(
-                        @{ NodeName = 'VM1'; }, @{ NodeName = 'VM2'; }, @{ NodeName = 'VM3'; }
-                    )
-                }
-                $testSnapshotName = 'Test Snapshot';
-                $fakeOffVM = [PSCustomObject] @{ State = 'Off' };
-                $fakeRunningVM = [PSCustomObject] @{ State = 'Running' };
-                Mock Get-VM -MockWith { return @($fakeRunningVM, $fakeOffVM, $fakeOffVM); }
+            { Checkpoint-Lab -ConfigurationData $configurationData -SnapshotName $testSnapshotName -ErrorAction Stop } | Should Throw;
+        }
 
-                Checkpoint-Lab -ConfigurationData $configurationData -SnapshotName $testSnapshotName -Force;
-
-                Assert-MockCalled New-LabVMSnapshot -ParameterFilter { $SnapshotName -eq $testSnapshotName };
+        It 'Snapshots VMs when there are running VMs and -Force is specified' {
+            $configurationData = @{
+                AllNodes = @(
+                    @{ NodeName = 'VM1'; }, @{ NodeName = 'VM2'; }, @{ NodeName = 'VM3'; }
+                )
             }
+            $testSnapshotName = 'Test Snapshot';
+            $fakeOffVM = [PSCustomObject] @{ State = 'Off' };
+            $fakeRunningVM = [PSCustomObject] @{ State = 'Running' };
+            Mock Get-VM -MockWith { return @($fakeRunningVM, $fakeOffVM, $fakeOffVM); }
+
+            Checkpoint-Lab -ConfigurationData $configurationData -SnapshotName $testSnapshotName -Force;
+
+            Assert-MockCalled New-LabVMSnapshot -ParameterFilter { $SnapshotName -eq $testSnapshotName };
+        }
 
     } #end InModuleScope
 
