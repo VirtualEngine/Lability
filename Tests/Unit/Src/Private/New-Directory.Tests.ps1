68e617061ae1f4716b98949cffc7cc5e110d8e09
Christian Sandfeld <csandfeld@users.noreply.github.com>
2017-11-08 13:55:05 +0100

    Minor bug in New-Directory and pester test for it (#267)
    * Did not test command output generated at point when target did not exist
    * Fixes bug when using array of paths, and adds missing tests to catch this.

--- Tests/Unit/Src/Private/New-Directory.Tests.ps1
+++ Tests/Unit/Src/Private/New-Directory.Tests.ps1
@@ -11,6 +11,7 @@ Describe 'Unit\Src\Private\New-Directory' {
 
         ## Need to resolve actual filesystem path for System.IO.DirectoryInfo calls
         $testDirectoryPath = "$((Get-PSdrive -Name TestDrive).Root)\New-Directory";
+        $testDirectoryPath2 = "$((Get-PSdrive -Name TestDrive).Root)\New-Directory2";
 
         It 'Returns a "System.IO.DirectoryInfo" object if target "Path" already exists' {
             Remove-Item -Path 'TestDrive:\New-Directory' -Force -ErrorAction SilentlyContinue;
@@ -42,9 +43,9 @@ Describe 'Unit\Src\Private\New-Directory' {
         It 'Returns a "System.IO.DirectoryInfo" object if target "DirectoryInfo" does not exist' {
             Remove-Item -Path 'TestDrive:\New-Directory' -Force -ErrorAction SilentlyContinue;
             Test-Path -Path $testDirectoryPath | Should Be $false;
-            New-Directory -Path $testDirectoryPath;
+            $output = New-Directory -Path $testDirectoryPath;
             Test-Path -Path $testDirectoryPath | Should Be $true;
-            (New-Directory -Path $testDirectoryPath) -is [System.IO.DirectoryInfo] | Should Be $true;
+            $output -is [System.IO.DirectoryInfo] | Should Be $true;
         }
 
         It 'Creates target "DirectoryInfo" if it does not exist' {
@@ -53,6 +54,46 @@ Describe 'Unit\Src\Private\New-Directory' {
             $directoryInfo = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList $testDirectoryPath;
             $directoryInfo | New-Directory;
             Test-Path -Path $testDirectoryPath | Should Be $true;
+        }
+
+        It 'Creates target "Paths" (plural) from parameter input if they do not exist' {
+            Remove-Item -Path 'TestDrive:\New-Directory','TestDrive:\New-Directory2' -Force -ErrorAction SilentlyContinue;
+            Test-Path -Path $testDirectoryPath | Should Be $false;
+            Test-Path -Path $testDirectoryPath2 | Should Be $false;
+            New-Directory -Path $testDirectoryPath, $testDirectoryPath2;
+            Test-Path -Path $testDirectoryPath | Should Be $true;
+            Test-Path -Path $testDirectoryPath2 | Should Be $true;
+        }
+
+        It 'Creates target "Paths" (plural) from pipeline input if they do not exist' {
+            Remove-Item -Path 'TestDrive:\New-Directory','TestDrive:\New-Directory2' -Force -ErrorAction SilentlyContinue;
+            Test-Path -Path $testDirectoryPath | Should Be $false;
+            Test-Path -Path $testDirectoryPath2 | Should Be $false;
+            $testDirectoryPath, $testDirectoryPath2 | Foreach-Object { [pscustomobject]@{ Path = $_ } } | New-Directory;
+            Test-Path -Path $testDirectoryPath | Should Be $true;
+            Test-Path -Path $testDirectoryPath2 | Should Be $true;
+        }
+
+        It 'Creates target "DirectoryInfos" (plural) from parameter input if they do not exist' {
+            Remove-Item -Path 'TestDrive:\New-Directory','TestDrive:\New-Directory2' -Force -ErrorAction SilentlyContinue;
+            $directoryInfo = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList $testDirectoryPath;
+            $directoryInfo2 = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList $testDirectoryPath2;
+            Test-Path -Path $testDirectoryPath | Should Be $false;
+            Test-Path -Path $testDirectoryPath2 | Should Be $false;
+            New-Directory -InputObject $directoryInfo, $directoryInfo2
+            Test-Path -Path $testDirectoryPath | Should Be $true;
+            Test-Path -Path $testDirectoryPath2 | Should Be $true;
+        }
+
+        It 'Creates target "DirectoryInfos" (plural) from pipeline input if they do not exist' {
+            Remove-Item -Path 'TestDrive:\New-Directory','TestDrive:\New-Directory2' -Force -ErrorAction SilentlyContinue;
+            $directoryInfo = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList $testDirectoryPath;
+            $directoryInfo2 = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList $testDirectoryPath2;
+            Test-Path -Path $testDirectoryPath | Should Be $false;
+            Test-Path -Path $testDirectoryPath2 | Should Be $false;
+            $directoryInfo, $directoryInfo2 | New-Directory;
+            Test-Path -Path $testDirectoryPath | Should Be $true;
+            Test-Path -Path $testDirectoryPath2 | Should Be $true;
         }
 
     } #end InModuleScope
